ed4f1e5ce85830355b0d95f9edd6d45673d29d92
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-10-30 01:02:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
-const { mongoUri, port, crawlUrl } = require('./config');
 const express = require('express');
 const mongoose = require('mongoose');
 const xsRoutes = require('./routes/xsRoutes');
 
 const app = express();
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log(`🔹 [Backend] Request ${req.method} ${req.url}`);
-  next();
-});
-app.use('/api', xsRoutes);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// Mount route
+app.use('/api/xs', xsRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
-  .catch(err => console.log('❌ MongoDB connection error:', err));
+  .catch(err => console.error(err));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(8080, () => console.log('Server running on port 8080'));
+
 
