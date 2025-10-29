af9917801ca0855ac7d3bbd304d0b791aed613ad
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-10-30 01:02:45 +0700

    Update xsRoutes.js

--- routes/xsRoutes.js
+++ routes/xsRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getAllResults, updateResults } = require('../controllers/xsController');
 
-router.get('/results', getAllResults);
-router.post('/update', updateResults);
+router.get('/results', getAllResults);      // GET /api/xs/results
+router.post('/update', updateResults);      // POST /api/xs/update
 
 module.exports = router;
