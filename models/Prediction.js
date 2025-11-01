ffa797cef9013a4885aa50d55c5529a8cacfbe1d
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-11-01 15:00:16 +0700

    Update Prediction.js

--- models/Prediction.js
+++ models/Prediction.js
@@ -4,11 +4,12 @@ const mongoose = require('mongoose');
 
 const chiTietGocSchema = new mongoose.Schema({ number: String, positionInPrize: Number, tram: String, chuc: String, donvi: String, weight: { type: Number, default: 1 } }, { _id: false });
 const ketQuaPhuongPhapSchema = new mongoose.Schema({ topTram: [String], topChuc: [String], topDonVi: [String], chiTietGoc: [chiTietGocSchema] }, { _id: false });
+const consensusAnalysisSchema = new mongoose.Schema({ predictedWinCount: Number, potentialNumbers: [String] }, { _id: false });
 
-// <<< SỬA LẠI SCHEMA NÀY CHO ĐÚNG VỚI HÀM CỦA BẠN >>>
+// --- Schema MỚI cho phương pháp Loại trừ Nhóm ---
 const groupExclusionAnalysisSchema = new mongoose.Schema({
-  potentialNumbers: [String],
-  excludedPatternCount: Number,
+  potentialNumbers: [String], // Các bộ số sống sót sau khi lọc
+  excludedPatternCount: Number, // Đã loại bỏ bao nhiêu mẫu hình
 }, { _id: false });
 
 const predictionSchema = new mongoose.Schema({
@@ -19,12 +20,13 @@ const predictionSchema = new mongoose.Schema({
   ketQuaChiTiet: { type: Map, of: ketQuaPhuongPhapSchema },
   diemTinCay: { type: Map, of: Number },
   intersectionAnalysis: { tram: mongoose.Schema.Types.Mixed, chuc: mongoose.Schema.Types.Mixed, donvi: mongoose.Schema.Types.Mixed },
-  groupExclusionAnalysis: groupExclusionAnalysisSchema, // Sử dụng schema đã sửa
+  consensusAnalysis: consensusAnalysisSchema,
+
+  // --- TRƯỜNG MỚI ---
+  groupExclusionAnalysis: groupExclusionAnalysisSchema,
+
   danhDauDaSo: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }
 }, { versionKey: false });
 
-// Loại bỏ ModelState không còn cần thiết nữa
-const PredictionModel = mongoose.model('Prediction', predictionSchema);
-
-module.exports = { Prediction: PredictionModel }; // Chỉ export Prediction
+module.exports = mongoose.model('Prediction', predictionSchema);
