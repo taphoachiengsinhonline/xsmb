7cda30a87dfe9316352ba1e615827b6b66a864dc
taphoachiengsinhonline <maytinhthaikhang@gmail.com>
2025-11-01 15:38:14 +0700

    Update Prediction.js

--- models/Prediction.js
+++ models/Prediction.js
@@ -4,12 +4,10 @@ const mongoose = require('mongoose');
 
 const chiTietGocSchema = new mongoose.Schema({ number: String, positionInPrize: Number, tram: String, chuc: String, donvi: String, weight: { type: Number, default: 1 } }, { _id: false });
 const ketQuaPhuongPhapSchema = new mongoose.Schema({ topTram: [String], topChuc: [String], topDonVi: [String], chiTietGoc: [chiTietGocSchema] }, { _id: false });
-const consensusAnalysisSchema = new mongoose.Schema({ predictedWinCount: Number, potentialNumbers: [String] }, { _id: false });
 
-// --- Schema MỚI cho phương pháp Loại trừ Nhóm ---
 const groupExclusionAnalysisSchema = new mongoose.Schema({
-  potentialNumbers: [String], // Các bộ số sống sót sau khi lọc
-  excludedPatternCount: Number, // Đã loại bỏ bao nhiêu mẫu hình
+  potentialNumbers: [String],
+  excludedPatternCount: Number,
 }, { _id: false });
 
 const predictionSchema = new mongoose.Schema({
@@ -20,13 +18,10 @@ const predictionSchema = new mongoose.Schema({
   ketQuaChiTiet: { type: Map, of: ketQuaPhuongPhapSchema },
   diemTinCay: { type: Map, of: Number },
   intersectionAnalysis: { tram: mongoose.Schema.Types.Mixed, chuc: mongoose.Schema.Types.Mixed, donvi: mongoose.Schema.Types.Mixed },
-  consensusAnalysis: consensusAnalysisSchema,
-
-  // --- TRƯỜNG MỚI ---
   groupExclusionAnalysis: groupExclusionAnalysisSchema,
-
   danhDauDaSo: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }
 }, { versionKey: false });
 
+// Quay lại cách export cũ vì không còn ModelState
 module.exports = mongoose.model('Prediction', predictionSchema);
